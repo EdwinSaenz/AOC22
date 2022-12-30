5bd161a0ea124efeb340503e8a9a676ae74c43aa
Aaron Saenz <aaron@saenz.soy>
2022-12-29 20:35:32 -0600

    Completed day 1

---
+++ Day 1/Program.fs
@@ -0,0 +1,35 @@
+﻿open System
+open System.IO
+
+let fileName = "input.txt"
+
+// Find the elf carrying most calories. Print calories
+let (max, _) = 
+    File.ReadAllLines fileName
+    |> Seq.fold (fun (max, currentMax) line ->
+        let mutable result = 0
+        if Int32.TryParse(line, &result) then
+            (max, currentMax + result)
+        elif max < currentMax then
+            (currentMax, 0)
+        else
+            (max, 0))
+        (0, 0)
+
+printfn "max: %i" max
+
+// Find the top three elves carrying most calories. Print their sum
+let sum =
+    File.ReadAllLines fileName
+    |> Seq.fold (fun maxes line -> 
+        let mutable result = 0
+        if Int32.TryParse(line, &result) then
+            [maxes.Head + result] @ maxes.Tail
+        else
+            [0] @ maxes)
+        [0]
+    |> Seq.sortDescending
+    |> Seq.take 3
+    |> Seq.sum
+
+printfn "sum of top 3: %i" sum
